e705d6753ae6f17b27e3fd01b312a6bb83f401d7
huyanhdo <86727493+huyanhdo@users.noreply.github.com>
2023-02-15 20:06:54 +0700

    add signup data

--- Screens/SignupPage.js
+++ Screens/SignupPage.js
@@ -115,6 +115,8 @@ const LoginPage = () => {
         email: email,
         name: name,
         password: password,
+        birthday:birthday,
+        gender:gender
       };
 
       const res = signupApi.post(data);
